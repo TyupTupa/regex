74f3ce1a26d48c636cceb6a6feaaa4c8f3ea43b7
TyupTupa <marysik1596@gmail.com>
2022-11-28 18:11:33 +0300

    hoho

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ def get_info_from_url(url):
     with urllib.request.urlopen(url) as resp:
         html = resp.read()
         file = html.decode("utf8")
+        # f = open('file.html','w')
+        # f.write(file)
+        # f.close()
         return file
 
 
-def regex(file):
+def regex(html):
     reg_link = r"[\"\']https?\:\/\/[^\"\']*[\"\']"
     reg_file = r"(?<=\w\/)[^\/\\\n:\*\?\<\>|\+]+\.[a-zA-Z]{1,4}(?=[\?\/\"\'])"
     result_links = re.findall(reg_link, file)
+    # print(result_links)
     result = []
     for link in result_links:
         print(link, re.findall(reg_file, link))
@@ -27,3 +31,4 @@ result = regex(file)
 
 for cur_file in result:
     print(cur_file)
+
